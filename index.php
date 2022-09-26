4e92ae5e4be399982b46ab9cb64efe7aff74ee61
moreira-g21 <99353497+moreira-g21@users.noreply.github.com>
2022-09-26 07:36:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en"><head> <meta charset="UTF-8"> 
 <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title></head><body>
+  <title>Document</title></head>
+   <body>
    <form action="form.php" method="post"> 
-   <p>Idr: <input type="number" name="ident"> </p>
-   <p>Nome do Funcionário: <input type="text" name="nomefun"></p>
-   <p>Emprego: <input type="text" name="empreg"></p>
-   <p>Salário: <input type="number" name="salar"></p> 
-   <p>Qtd Dependentes: <input type="text" name="depend"> </p>
-   <button type="submit"> Cadastrar </button> 
+      <p> Id: <input type="number" name="ident"> </p>
+      <p>Nome do Funcionário: <input type="text" name="nomefun"> </p>
+      <p>Emprego: <input type="text" name="empreg"> </p>
+      <p>Salário: <input type="number" name="salar"> </p>
+      <p>Qtd Dependentes: <input type="text" name="depend"> </p>
+       <button type="submit"> Cadastrar </button> 
    </form>
    </body>
    </html>
