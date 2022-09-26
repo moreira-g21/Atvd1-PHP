dc5310ddee22877a258dd5702138452e244b5029
moreira-g21 <99353497+moreira-g21@users.noreply.github.com>
2022-09-26 07:37:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en"><head> <meta charset="UTF-8"> 
 <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title></head>
+  <title>Document</title>
+  </head>
    <body>
    <form action="form.php" method="post"> 
       <p> Id: <input type="number" name="ident"> </p>
